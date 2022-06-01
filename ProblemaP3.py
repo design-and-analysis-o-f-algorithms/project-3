2cf8f9d831500c7dd373f258b83ec1799422a923
Juan Alegría <juanszalegria@gmail.com>
2022-06-01 16:44:38 -0500

    Perf: optimize code structures

--- ProblemaP3.py
+++ ProblemaP3.py
@@ -9,8 +9,9 @@ def decoder(data):
     appear, endIndex = [], 0
     # Se recorre de forma inversa la entrada y se agrega cada caracter nuevo encontrado
     for i in range(len(data)-1, -1, -1):
-        if not(data[i] in appear):
-            appear.append(data[i])
+        c = data[i]
+        if c not in appear:
+            appear.append(c)
             endIndex = i
     # Los caracteres encontrados se invierten dando el orden en el cual se eliminan
     appear.reverse()
@@ -18,24 +19,21 @@ def decoder(data):
     originalString = coder(data, appear, endIndex)
     if originalString == "NO EXISTE":
         return "NO EXISTE"
-    return originalString + ' ' + (''.join(appear))
+    return originalString + " " + "".join(appear)
 
 
 # Función que cifra diferentes cadenas hasta encontrar una cuyo cifrado concuerde con la entrada
 def coder(data, orden, endIndex):
     solution, originalString = "", ""
-    while solution != data:  # Mientras que la solución no concuerde con la entrada se sigue probando con strings más largos
-        if endIndex == (len(data)):
-            originalString = "NO EXISTE"
-            break
+    while solution != data:  # Mientras la solución no concuerde con la entrada se sigue probando con strings más largos
+        if endIndex == len(data):
+            return "NO EXISTE"
 
-        solution = data[0:endIndex+1]
-        removeString = solution
+        endIndex += 1
+        originalString = solution = removeString = data[0:endIndex]
         for i in orden:
             removeString = removeString.replace(i, '')
-            solution = solution+removeString
-        originalString = data[0:endIndex+1]
-        endIndex = endIndex+1
+            solution += removeString
     return originalString
 
 
@@ -43,7 +41,7 @@ def main():
     input = sys.stdin.readline
     numberCases = int(input())  # Número de casos de prueba
     solutions = []
-    for c in range(numberCases):
+    for _ in range(numberCases):
         data = input().strip()
         # Se llama a la función solución con los datos anteriores
         sol = decoder(data)
